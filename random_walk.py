d40c90bef0c1e13878cea0c21e90fac8cbb8e2fc
tnoikwms <53480622+tnoikwms@users.noreply.github.com>
2021-05-03 15:26:45 +0900

    Update random_walk.py

--- random_walk.py
+++ random_walk.py
@@ -1,65 +1,48 @@
-from os import W_OK
 import numpy as np
 import matplotlib as matp
 import matplotlib.pyplot as plt
+import math
 import random
 from matplotlib.animation import FuncAnimation
 import matplotlib.cm as cm
 import csv
 import pprint
 
-#ここクラス
-class randomwalk:
-    def __init__(self,N,particle):
-        self.fig = plt.figure(figsize=(6.4,6.4),dpi=100)
-        #self.a = self.fig.add_subplot(1,1,1,title = "tracking")
-        self.col = self.fig.add_subplot(1,1,1,title = "distribution")
-        self.n = N
+fig = plt.figure(figsize=(6.4,6.4),dpi=100)
+ax = fig.add_subplot(1,1,1,title = "tracking",facecolor="0")
+p1, = ax.plot([], [],'.',color='0.5',alpha=0.5)
+ax.set_xlim(-30,30)
+ax.set_ylim(-30,30)
+
+class tracking():
+    def __init__(self,particle,steps):
+        self.n = steps
         self.p = particle
-        self.totalsave = np.zeros((self.n+1,2),dtype=int)
-        self.last = np.zeros([2],dtype = int)
-    #複数粒子の分布を調べたい
-    def total(self):
-        for j in range(0,self.p):
-            self.x = np.zeros([2],dtype = int)
-            self.tot = self.x
-            for i in range(0,self.n):
-                r = random.randint(-1,1)
-                direction = random.randint(0,1)
-                if direction == 0:
-                    self.x[0] += r
-                else:
-                    self.x[1] += r
-                if i == self.n-1:
-                    self.last = np.vstack(([self.last,self.x]))
-                self.tot = np.vstack(([self.tot, self.x]))
-            self.totalsave = np.hstack((self.totalsave,self.tot))
-
-    #追跡のアニメーションを作る
-    def anima(self):
-
-    #csvに出力
-    def output(self):
-        np.savetxt('particle_tracking.csv',self.totalsave,fmt='%d')
-    #分布をヒストグラムとして表示する
-    def hist(self):
-        self.h = self.col.hist2d(self.last[:,0],self.last[:,1],(320,320),cmap=cm.jet)
-        self.h[3].set_clim(0,3)
-        self.fig.colorbar(self.h[3])
-    #表示
-    def show(self):
-        print(self.last)
-
-step = 100
-particle = 10
-
-rk = randomwalk(step,particle)
-
-#複数の粒子の分布
-rk.total()
+        self.pos_x = np.zeros([self.p])
+        self.pos_y = np.zeros([self.p])
+        self.tot_x = np.zeros([self.p])
+        self.tot_y = np.zeros([self.p])
+    def step(self):
+        for i in range(self.n):
+            theta = 2.0*np.pi*np.random.random(self.p)
+            self.pos_x = self.pos_x + np.cos(theta)*np.random.random(self.p)
+            self.pos_y = self.pos_y + np.sin(theta)*np.random.random(self.p)
+            self.tot_x = np.vstack([self.tot_x,self.pos_x])
+            self.tot_y = np.vstack([self.tot_y,self.pos_y])
+        tot_x = self.tot_x
+        tot_y = self.tot_y
+        return tot_x,tot_y
 
 
-#rk.output()
-#rk.hist()
-#plt.show()
-#rk.anima()
+#この量だと重い，5分以上かかる
+particle = 12000
+steps = 2000
+
+tk = tracking(particle,steps)
+tot_x,tot_y = tk.step()
+
+def update(i):
+    p1.set_data((tot_x[i,:],tot_y[i,:]))
+    return ax
+ani = FuncAnimation(fig,update,frames= steps,interval = 10)
+ani.save('rw2d_anim_flow4.mp4', writer="ffmpeg",dpi=200)
